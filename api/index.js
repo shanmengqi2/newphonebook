453d5d154309af78eb94e291d8e016c20c8a2767
shanmengqi <shanmengqi@gmail.com>
2025-10-23 18:11:15 +0800

    必须行

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
 // Vercel serverless function entry point
 const app = require('../phonebook_backend/index')
 
-// Vercel expects a function that handles (req, res)
-module.exports = (req, res) => {
-  // Let Express handle the request
-  return app(req, res)
-}
+module.exports = app
